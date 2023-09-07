3d4e8b816f9408bbcb4314e9071bdc59f345c4eb
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-09-07 12:52:45 +0000

    updates

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // Module imports
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -15,7 +15,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Main"
           component={MainScreen}
--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,19 @@
 import { View, Text, Button } from "react-native";
 
 export function LoginScreen({ navigation }) {
+
+    // Handle navigation to main page
+    function handle_navigation() {
+        navigation.reset({index: 0, routes: [{name: 'Main'}]});
+    }
+
     return(
         <View>
             <Text>Login Screen</Text>
             <Button
             title="Main Page"
             onPress={() =>
-                navigation.navigate('Main')
+                handle_navigation()
             }
             />
         </View>
