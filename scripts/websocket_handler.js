6dc5da28b883b2a2b50f412cd75cf2de0db5a806
Superior125 <90008321+Superior126@users.noreply.github.com>
2025-01-06 19:36:37 -0500

    Fix reconnect bar bug

--- scripts/websocket_handler.js
+++ scripts/websocket_handler.js
@@ -191,6 +191,7 @@ export const WebSocketProvider = ({ children }) => {
         sendMessage,
         closeConnection,
         updateTypingStatus,
+        shouldReconnect,
       }}
     >
       {children}
